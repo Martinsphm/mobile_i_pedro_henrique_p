93b38163ce92465fb7b12db05b4872225a4206a5
Pedro Passos <pedroh.passosm7@gmail.com>
2024-03-14 08:21:39 -0300

    Aulas sobre metodos e funçoes

---
+++ aula03/metodos.js
@@ -0,0 +1,11 @@
+var txt = "hello amigos";
+
+console.log(txt.length);
+
+console.log(txt.slice(6,12));
+
+console.log(txt.replace("hello","olá"));
+
+let newtxt = txt.replace("hello","olá");
+
+console.warn(newtxt.toUpperCase())
